903eb72266f5039c7efe5439d888ca04975c4a41
Trevor Barron <barron.trevor@gmail.com>
2015-10-09 13:29:28 -0600

    adding caffe config files / scripts to run from command line

---
+++ caffenet.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+from caffe import layers as L, params as P, to_proto
+from caffe.proto import caffe_pb2
+
+# helper function for common structures
+
+def conv_relu(bottom, ks, nout, stride=1, pad=0, group=1):
+    conv = L.Convolution(bottom, kernel_size=ks, stride=stride,
+                                num_output=nout, pad=pad, group=group)
+    return conv, L.ReLU(conv, in_place=True)
+
+def fc_relu(bottom, nout):
+    fc = L.InnerProduct(bottom, num_output=nout)
+    return fc, L.ReLU(fc, in_place=True)
+
+def max_pool(bottom, ks, stride=1):
+    return L.Pooling(bottom, pool=P.Pooling.MAX, kernel_size=ks, stride=stride)
+
+def caffenet(lmdb, batch_size=256, include_acc=False):
+    print ("building net")
+    data, label = L.Data(source=lmdb, backend=P.Data.LMDB, batch_size=batch_size, ntop=2,
+        transform_param=dict(crop_size=227, mean_value=[104, 117, 123], mirror=True))
+
+    # the net itself
+    conv1, relu1 = conv_relu(data, 8, 32, stride=4)
+    conv2, relu2 = conv_relu(relu1, 4, 16, stride=2)
+    ip1 = L.InnerProduct(relu2, num_output=256)
+    relu3 = L.ReLU(ip1, in_place=True)
+    ip2 = L.InnerProduct(relu3, num_output=64)
+    loss = L.SoftmaxWithLoss(ip2, label)
+    
+    if include_acc:
+        acc = L.Accuracy(ip2, label)
+        return to_proto(loss, acc)
+    else:
+        return to_proto(loss)
+
+def make_net():
+    with open('train.prototxt', 'w') as f:
+        print(caffenet('/path/to/caffe-train-lmdb'), file=f)
+
+    with open('test.prototxt', 'w') as f:
+        print(caffenet('/path/to/caffe-val-lmdb', batch_size=50, include_acc=True), file=f)
+
+if __name__ == '__main__':
+    make_net()
---
+++ train_minecraft.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+$CAFFE_ROOT/build/tools/caffe train --solver=minecraft_solver.prototxt
